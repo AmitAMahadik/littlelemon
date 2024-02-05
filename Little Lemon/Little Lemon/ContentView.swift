0982731d80e3a6bec8d81c44e141c75cff06c746
Amit Mahadik <amahadik@voceracommunications.com>
2024-02-04 17:54:52 -0800

    welcome to my world

--- Little Lemon/Little Lemon/ContentView.swift
+++ Little Lemon/Little Lemon/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, My world!")
+            Text("Hello, Welcome to My world!")
         }
         .padding()
     }
